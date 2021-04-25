85bd893490b8b434b95cb86a9560fdb30d8a9971
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-04-25 10:18:56 +0200

    Added Java implementation primeFactorization(long l)

---
+++ java/number-theory/PrimeFactorization.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class PrimeFactorization {
+
+    /**
+     * This method returns the prime factorization of a number.
+     * @param l the number to be factorized
+     * @return an ArrayList representing the prime factorization.
+     */
+    public static ArrayList<Long> PrimeFactorization(long l)
+    {
+        ArrayList<Long> primeFactorization = new ArrayList();
+        long div = 1;
+        while (l != 1)
+        {
+            div++;
+
+            // if div divides l
+            if (l % div == 0)
+            {
+                primeFactorization.add(div);
+                l /= div;
+                div--;
+                continue;
+            }
+
+            if (div > Math.sqrt(l))
+            {
+                primeFactorization.add(l);
+                break;
+            }
+        }
+
+        return primeFactorization;
+    }
+}
