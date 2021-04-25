73ad1ff77336f89413de5a3c6e56407b936560db
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-04-25 10:46:22 +0200

    Fixed type in isPrime method

--- java/number-theory/IsPrime.java
+++ java/number-theory/IsPrime.java
@@ -10,7 +10,7 @@ public class IsPrime {
         if (n <= 1) return false;
         if (n == 2 || n == 3) return true;
         if (n % 2 == 0 || n % 3 == 0) return false;
-        for (int i = 5; i <= Math.Sqrt(n); i += 6)
+        for (int i = 5; i <= Math.sqrt(n); i += 6)
         {
             if (n % i == 0) return false;
             if (n % (i + 2) == 0) return false;
