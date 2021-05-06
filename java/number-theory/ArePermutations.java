d0d1709908490d06d633727e14e54ba78cc01535
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-05-06 18:09:50 +0200

    Added Java arePermuations(long n1, long n2) method

---
+++ java/number-theory/ArePermutations.java
@@ -0,0 +1,23 @@
+public class ArePermutations {
+
+    /**
+     * This method determines whether two integers are permuations of each other.
+     * @param n1 the first number.
+     * @param n2 the second number.
+     * @return true iff n1 is a permutation of n2.
+     */
+    public static boolean arePermutations(long n1, long n2)
+    {
+        int[] digits1 = new int[10];
+        int[] digits2 = new int[10];
+        for (char c1 : Long.toString(n1).toCharArray())
+        digits1[c1 - '0']++;
+        for (char c2 : Long.toString(n2).toCharArray())
+        digits2[c2 - '0']++;
+        for (int digit = 0; digit <= 9; digit++)
+        {
+            if (digits1[digit] != digits2[digit]) return false;
+        }
+        return true;
+    }
+}
