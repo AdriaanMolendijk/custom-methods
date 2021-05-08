8935b7ddcf562bd0fd6a1d3dad84773b7c304112
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-05-08 14:29:41 +0200

    Added is_palindrome(s) function python

---
+++ python/ispalindrome.py
@@ -0,0 +1,10 @@
+# This method determines whether a string is palindrome.
+# s the string on which to check the palindrome property.
+# true iff s is palindrome.
+def is_palindrome(s):
+    l = len(s)
+    if l <= 1:
+        return True
+    c1 = s[0]
+    c2 = s[-1]
+    return c1 == c2 and is_palindrome(s[1:-2])
