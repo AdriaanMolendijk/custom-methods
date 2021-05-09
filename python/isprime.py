947580fc4e0e95bb7bf36e9ff3a49c23e4760f1a
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-05-09 09:30:31 +0200

    Added python isprime method

---
+++ python/isprime.py
@@ -0,0 +1,17 @@
+import math
+
+
+# This method determines whether an integer is prime or not.
+# n the number on which to perform the primality test.
+# return true iff n is prime.
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2 or n == 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    for i in range(5, math.sqrt(n) + 1, 6):
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+    return True
