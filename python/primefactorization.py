752cbc69f4492a230de78e54701e629d290e70e8
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-05-09 09:24:05 +0200

    Added python primefactorization function

---
+++ python/primefactorization.py
@@ -0,0 +1,21 @@
+import math
+
+
+# This method returns the prime factorization of a number.
+# l the number to be factorized
+# return an ArrayList representing the prime factorization.
+def prime_factorization(n):
+    factorization = []
+    div = 1
+    while n != 1:
+        if n % div == 0:
+            factorization.append(div)
+            div = div - 1
+            continue
+
+        if div > math.sqrt(n):
+            factorization.append(div)
+            break
+
+
+
